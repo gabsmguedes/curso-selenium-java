90f5106ef05e2dd038bef2174c8a763428e2d174
Gabriel <ga.mouraguedes@gmail.com>
2021-01-30 19:36:02 -0300

    Id na pagina de login e template
    Inserido alguns ids necessários no formulário de login e template para realizar o teste de login.

--- src/test/java/br/com/alura/leilao/login/LoginTest.java
+++ src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -1,2 +1,33 @@
-package br.com.alura.leilao.login;public class LoginTest {
+package br.com.alura.leilao.login;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+
+public class LoginTest {
+
+    static WebDriver webDriver;
+
+    @Test
+    public void logarComDadosValidos() {
+        String operaBinary = "D:\\Programas Instalados\\Opera\\73.0.3856.345\\opera.exe";
+        WebDriverManager.operadriver().setup();
+        OperaOptions options = new OperaOptions();
+        options.setBinary(operaBinary);
+        webDriver = new OperaDriver(options);
+
+        webDriver.get("http://localhost:8080/login");
+        webDriver.findElement(By.id("username")).sendKeys("fulano");
+        webDriver.findElement(By.id("password")).sendKeys("pass");
+        webDriver.findElement(By.id("login-form")).submit();
+
+        Assert.assertFalse(webDriver.getCurrentUrl().equals("http://localhost:8080/login"));
+        Assert.assertEquals("fulano", webDriver.findElement(By.id("usuario-logado")).getText());
+        webDriver.quit();
+    }
+
 }
