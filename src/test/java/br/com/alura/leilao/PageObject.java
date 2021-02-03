14d88568b1cbb944fc0d470e2191db678bb7aff5
Gabriel <ga.mouraguedes@gmail.com>
2021-02-03 20:22:51 -0300

    Adicionando times
    Times de espera para carregar pagina

--- src/test/java/br/com/alura/leilao/PageObject.java
+++ src/test/java/br/com/alura/leilao/PageObject.java
@@ -3,6 +3,8 @@ package br.com.alura.leilao;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class PageObject {
 
     protected WebDriver webDriver;
@@ -10,6 +12,7 @@ public class PageObject {
     public PageObject(WebDriver webDriver) {
         System.setProperty("webdriver.chrome.driver", "D:\\Projetos\\curso-selenium-java\\src\\test\\drivers\\chrome\\chromedriver.exe");
         this.webDriver = webDriver == null ? new ChromeDriver() : webDriver;
+        this.webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS).pageLoadTimeout(10, TimeUnit.SECONDS);
     }
 
     public void fecharBrowser() {
